2533d3ad7f3596d40e422cb41008dfe231cb8202
Franky Martin <fmartin@neutral.io>
2016-12-08 13:16:07 -0500

    PROD-70, checking permissions against framework settings functionality

--- tests/test_oauth2_authorization.py
+++ tests/test_oauth2_authorization.py
@@ -24,9 +24,10 @@ class TestBasicAuthorization(NIOTestCase):
             context.client_id = "foo_client"
             context.validate_uri = "https://myverifyinguri?access=%s"
             context.permissions = {
-                "user1@company.com": ["services.view", "blocks.view"],
-                "domain.com": ["services.*", "*.view"],
-                "user3@company.com": []
+                "user1@company.com":
+                    {"services": "r", "blocks": "r", "instance": ""},
+                "user3@company.com":
+                    {"services": "", "blocks": "", "instance": ""}
             }
             return context
         else:
@@ -35,31 +36,22 @@ class TestBasicAuthorization(NIOTestCase):
     def test_basic_permissions(self):
         """ Tests the basic permission matching """
         user = User("user1@company.com")
-        Authorizer.authorize(user, SecureTask("services", "view"))
-        Authorizer.authorize(user, SecureTask("blocks", "view"))
+        Authorizer.authorize(user, SecureTask("services", "read"))
+        Authorizer.authorize(user, SecureTask("blocks", "read"))
         with self.assertRaises(Unauthorized):
-            Authorizer.authorize(user, SecureTask("modules", "view"))
-
-    def test_domain_and_wildcard_permissions(self):
-        """ Tests that we can define permissions with wildcards """
-        user = User("user2@domain.com")
-        Authorizer.authorize(user, SecureTask("services", "view"))
-        Authorizer.authorize(user, SecureTask("services", "edit"))
-        Authorizer.authorize(user, SecureTask("blocks", "view"))
-        with self.assertRaises(Unauthorized):
-            Authorizer.authorize(user, SecureTask("blocks", "edit"))
+            Authorizer.authorize(user, SecureTask("services", "execute"))
 
     def test_no_permissions(self):
         """ Tests that a user with no permissions is Unauthorized """
         user = User("user3@company.com")
         with self.assertRaises(Unauthorized):
-            Authorizer.authorize(user, SecureTask("blocks", "view"))
+            Authorizer.authorize(user, SecureTask("blocks", "read"))
 
     def test_nonexistent_user(self):
         """ Tests that a user who doesn't exist is Unauthorized """
         user = User("not a user")
         with self.assertRaises(Unauthorized):
-            Authorizer.authorize(user, SecureTask("blocks", "view"))
+            Authorizer.authorize(user, SecureTask("blocks", "read"))
 
     def test_invalid_authorize(self):
         """ Tests that authorize must be called with the right types """
